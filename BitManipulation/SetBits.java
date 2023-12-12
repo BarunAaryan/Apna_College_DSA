5c9c93dfe7db39262c4a0cc3d4b9d51d72d21532
Barun Aaryan <barunaryan1@gmail.com>
2023-12-12 19:26:41 +0530

    Count Set Bits

---
+++ BitManipulation/SetBits.java
@@ -0,0 +1,19 @@
+public class SetBits {
+    //Count Set Bits in a Number
+    public static int countSetBits(int n){
+        int count =0;
+        while(n>0){
+            if((n & 1) !=0){ //Check our LSB
+                count++;
+            }
+            //right shift
+            n= n>>1;
+        }
+         return count;
+    }
+    public static void main(String[] args) {
+        System.out.println(countSetBits(10));
+    }
+   
+
+}
